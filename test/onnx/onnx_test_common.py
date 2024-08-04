dd5f94cae20cbde58720ec2a1a86c8777d05458f
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-08-05 01:34:43 +0800

    [BE][Easy] use `pathlib.Path` instead of `dirname` / `".."` / `pardir` (#129374)
    Changes by apply order:
    
    1. Replace all `".."` and `os.pardir` usage with `os.path.dirname(...)`.
    2. Replace nested `os.path.dirname(os.path.dirname(...))` call with `str(Path(...).parent.parent)`.
    3. Reorder `.absolute()` ~/ `.resolve()`~ and `.parent`: always resolve the path first.
    
        `.parent{...}.absolute()` -> `.absolute().parent{...}`
    
    4. Replace chained `.parent x N` with `.parents[${N - 1}]`: the code is easier to read (see 5.)
    
        `.parent.parent.parent.parent` -> `.parents[3]`
    
    5. ~Replace `.parents[${N - 1}]` with `.parents[${N} - 1]`: the code is easier to read and does not introduce any runtime overhead.~
    
        ~`.parents[3]` -> `.parents[4 - 1]`~
    
    6. ~Replace `.parents[2 - 1]` with `.parent.parent`: because the code is shorter and easier to read.~
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129374
    Approved by: https://github.com/justinchuby, https://github.com/malfet
    ghstack-source-id: f207a4bd5425f6818561eb8eb77900392301618d

--- test/onnx/onnx_test_common.py
+++ test/onnx/onnx_test_common.py
@@ -46,8 +46,7 @@ _InputArgsType = Optional[
 _OutputsType = Sequence[_NumericType]
 
 onnx_model_dir = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    os.pardir,
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
     "repos",
     "onnx",
     "onnx",
@@ -55,11 +54,7 @@ onnx_model_dir = os.path.join(
     "test",
     "data",
 )
-
-
 pytorch_converted_dir = os.path.join(onnx_model_dir, "pytorch-converted")
-
-
 pytorch_operator_dir = os.path.join(onnx_model_dir, "pytorch-operator")
 
 
