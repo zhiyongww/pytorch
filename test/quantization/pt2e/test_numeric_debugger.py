f8d6a4544a6bc32f4b19e213c8143a4343168829
Jerry Zhang <jerryzh168@gmail.com>
2024-09-03 12:55:57 -0700

    Fix two tests related to export metadata preservation
    Summary:
    This is a follow up of https://github.com/pytorch/pytorch/pull/134040 to fix the remaining
    tests that still breaks right now, I think we'd need to fix something in export
    
    Test Plan:
    python test/test_quantization -k TestNumericDebugger
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 0a43f911767dfda3e28001ae3285d7fc949bd6a9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/134776

--- test/quantization/pt2e/test_numeric_debugger.py
+++ test/quantization/pt2e/test_numeric_debugger.py
@@ -116,22 +116,18 @@ class TestNumericDebugger(TestCase):
 
         self.assertEqual(debug_handle_map, debug_handle_map_ref)
 
-    @unittest.skip("All nodes' meta are preserved but get_attr nodes' meta are wrong.")
     def test_re_export_preserve_handle(self):
         m = TestHelperModules.Conv2dThenConv1d()
         example_inputs = m.example_inputs()
-        m = capture_pre_autograd_graph(m, example_inputs)
+        m = torch.export.export_for_training(m, example_inputs)
         generate_numeric_debug_handle(m)
 
         debug_handle_map_ref = _extract_debug_handles(m)
-        m_export = capture_pre_autograd_graph(m, example_inputs)
+        m_export = torch.export.export_for_training(m, example_inputs)
         debug_handle_map = _extract_debug_handles(m_export)
 
         self.assertEqual(debug_handle_map, debug_handle_map_ref)
 
-    @unittest.skip(
-        "All nodes' meta are preserved but the first arg for the first node seems to be dropped"
-    )
     def test_run_decompositions_preserve_handle(self):
         m = TestHelperModules.Conv2dThenConv1d()
         example_inputs = m.example_inputs()
